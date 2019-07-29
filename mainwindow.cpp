d95efd0d3e1d2535c0111cce5349b21ea09247c7
Sanya Zhitnik <sanya007ua@gmail.com>
2019-07-29 21:54:39 +0300

    Paint

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    int qq = 123;
+    QString s = QString::number(qq);
+
+    QPainter painter(this);
+       //painter.drawPoint(100,200);
+    painter.drawRect(QRect(100,100,100,100));
+    painter.drawText(150,150,s);
+
+}
